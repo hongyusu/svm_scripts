b65218a5806b5bac8f15f553a490dd9abc395d8b
hongyusu <hongyu.su.uh@gmail.com>
2012-10-26 13:53:52 +0300

    2610

--- run_SVM.m
+++ run_SVM.m
@@ -113,7 +113,7 @@ perf=[perf;[acc,vecacc,pre,rec,f1,auc1,auc2]];perf
 %
 %------------
 % parameter selection
-svm_cs=[0.01,0.1,1,5,10];
+svm_cs=[0.01,0.1,0.5,1,5,10];
 Isel = randsample(1:size(K,2),ceil(size(K,2)*.03));
 IselTrain=Isel(1:ceil(numel(Isel)/3*2));
 IselTest=Isel(1:ceil(numel(Isel)/3));
