26c82f32f1c74e7d4998bf2d26fbddbb4cfab18d
hongyusu <hongyu.su.uh@gmail.com>
2012-10-31 15:59:33 +0200

    3110

--- autorun_SVM.sh
+++ autorun_SVM.sh
@@ -1,7 +1,7 @@
 
 
-#for inname in 'emotions' 'yeast' 'scene' 'enron' 'cal500' 'fp' 'cancer' 'medical' 'toy10' 'toy50'
-for inname in 'toy10'
+for inname in 'emotions' 'yeast' 'scene' 'enron' 'cal500' 'fp' 'cancer' 'medical' 'toy10' 'toy50'
+#for inname in 'toy10'
 do
     nohup matlab -nodesktop -nosplash -r "run_SVM '$inname'" &
     sleep 2s
